660d38bc978da267276291e752ece39952a85459
Rakesh Ayyaswami <rakeshta@gmail.com>
2025-12-09 00:30:10 +1100

    renamed web app

--- apps/webapp/tailwind.config.js
+++ apps/webapp/tailwind.config.js
@@ -11,13 +11,13 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './{src,pages,components,app}/**/*.{ts,tsx,js,jsx,html}',
-    '!./{src,pages,components,app}/**/*.{stories,spec}.{ts,tsx,js,jsx,html}',
-    //     ...createGlobPatternsForDependencies(__dirname)
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './{src,pages,components,app}/**/*.{ts,tsx,js,jsx,html}',
+        '!./{src,pages,components,app}/**/*.{stories,spec}.{ts,tsx,js,jsx,html}',
+        //     ...createGlobPatternsForDependencies(__dirname)
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
